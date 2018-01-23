d5a8bc8e5844f274c942a082c2271eee16fdb5f4
AdrienZ <adrienzaganelli@gmail.com>
2018-01-23 10:03:08 +0100

    Update project description

--- app/src/js/components/Me.js
+++ app/src/js/components/Me.js
@@ -19,8 +19,8 @@ export default (props) =>
         />
       }
       <p>
-        I’m a Paris-based Web developer and a 4th year student at <ExternalLink href="http://hetic.net/" title="HETIC, école web">HETIC</ExternalLink>.
-        I'm currently looking for a 6 months <ExternalLink href="mailto:adrienzaganelli@gmail.com" target="_blank" class="underline" title="adrienzaganelli@gmail.com">internship</ExternalLink>.
+        I’m a french Web developer and a 4th year student at <ExternalLink href="http://hetic.net/" title="HETIC, école web">HETIC</ExternalLink>.
+        I'm looking for a 6-month <ExternalLink href="mailto:adrienzaganelli@gmail.com" target="_blank" class="underline" title="adrienzaganelli@gmail.com">internship</ExternalLink>.
         Also available for any freelance projects, I'm open for job opportunities.
       </p>
     </div>
