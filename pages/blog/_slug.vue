58d59262d5fc5e3ce0a392679c35b66371e07e40
AdrienZ <adrienzaganelli@gmail.com>
2020-02-23 01:23:03 +0100

    handle cms image path

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -1,28 +1,63 @@
 <template>
-  <article class="page-post">
-    <nuxt-link to="/blog/">Retour au articles</nuxt-link>
+  <section class="page-post post">
+    <article class="post-content">
+      <nuxt-link to="/blog/">Retour au articles</nuxt-link>
 
-    <h1>{{post.title}}</h1>
-    <div v-html="$md.render(post.body)"></div>
-  </article>
+      <h1>{{post.title}}</h1>
+      <div v-html="$md.render(post.body)"></div>
+    </article>
+  </section>
 </template>
 
 <script>
 export default {
-  async asyncData({ params, payload, store }) {
+  async asyncData({ isDev, params, payload, store }) {
+    let post;
+
     if (payload) {
-      return { post: payload };
+      post = payload;
     } else if (store.state.blogPosts.length) {
-      return { post: store.getters.getPost(params.slug) };
+      post = store.getters.getPost(params.slug);
     }
+
+    post.body = post.body.split("](/medias/").join("](/zigzag-cms/api/medias/");
+
+    return { post };
   }
 };
 </script>
 
 <style lang="scss" scoped>
 .page-post {
+  background: #1a202c;
+  color: #fff;
+
+  a {
+    color: inherit;
+  }
+}
+
+.post-content {
   max-width: 580px;
   margin: auto;
-  padding: 0 20px;
+  padding: 10rem 20px;
+}
+
+.post /deep/ {
+  p,
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h5,
+  pre,
+  iframe {
+    margin-top: 2rem;
+  }
+
+  img {
+    max-width: 100%;
+  }
 }
 </style>
