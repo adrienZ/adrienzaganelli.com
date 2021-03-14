1496360ea5b8a625a9ee5a5f86b3d175091ebf1b
AdrienZ <adrienzaganelli@gmail.com>
2021-03-14 11:51:07 +0100

    fix mix content

--- mixins/withLazyImages.js
+++ mixins/withLazyImages.js
@@ -1,21 +1,24 @@
 import lazysizes from 'lazysizes';
 
 export const writeLazyWpImages = str => {
-  const imageRegExp = /(<figure class=\"wp-block-image\")(.*)(figure>)/gi
+  const imageRegExp = /<figure.*class\s*=\s*["'].*wp-block-image.*["']\s*>(.*)<\/figure>/gi
   return str.replace(imageRegExp, str => {
     let res = str
+    console.log('oy', res);
     res = res.replace('src="', 'data-src="')
+    res = res.replace('data-src="http://', 'data-src="https://' )
     res = res.replace('srcset="', 'data-srcset="')
     res = res.replace('class="wp-image-', 'class="lazyload wp-image-')
     return res
   })
 }
 export const writeLazyWpVideos = str => {
-  const videoRegExp = /(<figure class=\"wp-block-video\")(.*)(figure>)/gi
+  const videoRegExp = /<figure.*class\s*=\s*["'].*wp-block-video.*["']\s*>(.*)<\/figure>/gi
   return str.replace(videoRegExp, str => {
     let res = str
     res = res.replace('<video', '<video class="lazyload"' )
     res = res.replace('src="', 'data-src="' )
+    res = res.replace('data-src="http://', 'data-src="https://' )
     return res
   })
 }
