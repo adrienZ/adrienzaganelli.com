bbda0dcfce50dbbb9c1943a3950d8ff7ab62eb1f
Adrien Zaganelli <adrienzaganelli@gmail.com>
2023-11-02 11:56:04 +0100

    typo

--- plugins/rss-auto-discover.server.ts
+++ plugins/rss-auto-discover.server.ts
@@ -1,11 +1,11 @@
-export default defineNuxtPlugin((nuxtApp) => {
+export default defineNuxtPlugin(() => {
 	useHead({
 		link: [
 			{
 				rel: "alternate",
+				type: "application/rss+xml",
 				href: useAppConfig().rssFeedPath,
 				title: "Adrien Zaganelli",
-				type: "application/rss+xml",
 			},
 		],
 	});
