3a532487657d032f2e6565062b5d81d16d12651c
Adrien Zaganelli <adrienzaganelli@gmail.com>
2023-11-02 11:36:39 +0100

    legacy rss route

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,7 +13,7 @@ export default defineNuxtConfig({
 
 	nitro: {
 		prerender: {
-			routes: ["/feed.xml"],
+			routes: ["/feed.xml", "/rss.xml"],
 		},
 		// just for netlify
 		future: {
---
+++ server/routes/feed.xml.ts
@@ -0,0 +1,47 @@
+// https://mokkapps.de/blog/create-an-rss-feed-with-nuxt-3-and-nuxt-content-v2
+
+/**
+ * LEGACY ROUTE FOR EXISTING SUBSCRIBERS
+ */
+
+import { serverQueryContent, parseContent } from "#content/server";
+import RSS from "rss";
+
+export default defineEventHandler(async (event) => {
+	const feed = new RSS({
+		title: "Adrien Zaganelli Blog",
+		site_url: "https://adrienzaganelli.com",
+		feed_url: `https://adrienzaganelli.com/feed`,
+		image_url: "https://adrienzaganelli.com/favicon.ico",
+	});
+
+	const docs = await serverQueryContent(event)
+		.where({ draft: false })
+		.only([
+			"title",
+			"_path",
+			"date",
+			"media",
+			"description",
+			"excerpt",
+			"thumbnail_height",
+		])
+		.sort({ date: -1 })
+		.find();
+
+	const blogPosts = docs.filter((doc) => doc?._path?.includes("/blog"));
+
+	for (const doc of blogPosts) {
+		feed.item({
+			title: doc.title ?? "-",
+			url: `https://adrienzaganelli.com${doc._path}`,
+			date: doc.date,
+			description: doc.description,
+			author: "Adrien Zaganelli",
+		});
+	}
+
+	const feedString = feed.xml({ indent: true });
+	event.res.setHeader("content-type", "text/xml");
+	event.res.end(feedString);
+});
