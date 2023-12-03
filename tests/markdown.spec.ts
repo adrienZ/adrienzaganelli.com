c3a85418bcb5daa6df3bc6ffe3b149fa9884b937
Adrien Zaganelli <adrienzaganelli@gmail.com>
2023-12-03 23:37:30 +0100

    e2e tests

---
+++ tests/markdown.spec.ts
@@ -0,0 +1,19 @@
+import { test, expect } from "@playwright/test";
+
+test("Blog is rendering", async ({ page }) => {
+	await page.goto(
+		"/blog/free-google-analytics-alternative-quickstart-with-umami-analytics",
+	);
+	const article = await page.getByRole("article");
+	await expect(article).toContainText(
+		"As a European, I have witnessed Google Analytics become illegal",
+	);
+});
+
+test("Case study is rendering", async ({ page }) => {
+	await page.goto("/case-study/sketchfab");
+	const article = await page.getByRole("article");
+	await expect(article).toContainText(
+		"My mission was to support the web team during the launch of several major features",
+	);
+});
